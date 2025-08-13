1b2875d03babcac6cbd5a16318e6229014458fe8
RossLand <1145902985@qq.com>
2025-08-13 12:50:22 +0800

    初步实现搜索框、使用ng cli创建component并引用

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Home} from './home/home';
+import {HousingLocation} from './housing-location/housing-location';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    Home,
+    HousingLocation
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
