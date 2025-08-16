fbf062020431a99fc796cd40384adc380bd5ae4f
RossLand <1145902985@qq.com>
2025-08-17 00:53:17 +0800

    组件间传值，配合css，实现房屋信息的展示

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
 import {Home} from './home/home';
-import {HousingLocation} from './housing-location/housing-location';
 
 @Component({
   selector: 'app-root',
   imports: [
-    Home,
-    HousingLocation
+    Home
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {HousingLocation} from '../housing-location';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing">
+      <!-- src放入[]中代表 -->
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of the {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+    </section>
   `,
   styleUrl: './housing-location.css'
 })
-export class HousingLocation {
-
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
 }
+
