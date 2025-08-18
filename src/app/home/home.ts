8fe563bdad2c47ac081b381aeac69c9fa819440b
RossLand <1145902985@qq.com>
2025-08-18 11:08:48 +0800

    无数据时给出提示

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,6 +1,6 @@
 import {Component, inject} from '@angular/core';
 import {HousingLocationComponent} from '../housing-location/housing-location';
-import {NgForOf} from '@angular/common';
+import {CommonModule, NgForOf} from '@angular/common';
 import {HousingLocation} from '../housing-location';
 import {HousingService} from '../housingService';
 
@@ -8,6 +8,7 @@ import {HousingService} from '../housingService';
   selector: 'app-home',
   imports: [
     HousingLocationComponent,
+    CommonModule,
     NgForOf
   ],
   template: `
@@ -23,6 +24,25 @@ import {HousingService} from '../housingService';
 
       </app-housing-location>
     </section>
+
+    <!-- 当 filteredLocationList 为空时显示提示 -->
+    <p
+      *ngIf="filteredLocationList.length === 0"
+      style="
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    text-align: center;
+    font-style: italic;
+    color: #666;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  ">
+      No housing locations found. Try a different search term.
+    </p>
   `,
   styleUrl: './home.css'
 })
