4ab7feb774f1a0642607c774d0b440f5ee197ca9
RossLand <1145902985@qq.com>
2025-08-18 22:41:55 +0800

    更新ngFor和ngIf为@for和@if

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,6 +1,6 @@
 import {Component, inject} from '@angular/core';
 import {HousingLocationComponent} from '../housing-location/housing-location';
-import {CommonModule, NgForOf} from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {HousingLocation} from '../housing-location';
 import {HousingService} from '../housingService';
 
@@ -8,8 +8,7 @@ import {HousingService} from '../housingService';
   selector: 'app-home',
   imports: [
     HousingLocationComponent,
-    CommonModule,
-    NgForOf
+    CommonModule
   ],
   template: `
     <!-- <p> -->
@@ -20,29 +19,51 @@ import {HousingService} from '../housingService';
       <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
     </section>
     <section class="results">
-      <app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation">
+      <!-- <app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"> -->
 
-      </app-housing-location>
+      <!-- </app-housing-location> -->
+      @for (housingLocation of housingLocationList; track housingLocation.id) {
+        <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+      }
+
     </section>
 
     <!-- 当 filteredLocationList 为空时显示提示 -->
-    <p
-      *ngIf="filteredLocationList.length === 0"
-      style="
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 100%;
-    text-align: center;
-    font-style: italic;
-    color: #666;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  ">
-      No housing locations found. Try a different search term.
-    </p>
+    <!--   <p -->
+    <!--     *ngIf="filteredLocationList.length === 0" -->
+    <!--     style=" -->
+    <!--   position: fixed; -->
+    <!--   top: 50%; -->
+    <!--   left: 50%; -->
+    <!--   transform: translate(-50%, -50%); -->
+    <!--   width: 100%; -->
+    <!--   text-align: center; -->
+    <!--   font-style: italic; -->
+    <!--   color: #666; -->
+    <!--   margin: 0; -->
+    <!--   padding: 0; -->
+    <!--   box-sizing: border-box; -->
+    <!-- "> -->
+    <!--     No housing locations found. Try a different search term. -->
+    <!--   </p> -->
+
+    @if (filteredLocationList.length === 0) {
+      <p style="
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 100%;
+      text-align: center;
+      font-style: italic;
+      color: #666;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    ">
+        No housing locations found. Try a different search term!
+      </p>
+    }
   `,
   styleUrl: './home.css'
 })
